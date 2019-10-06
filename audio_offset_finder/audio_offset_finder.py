7313368b83c5f7ccf8f89227c83ffc4a88aaa8c0
Ben Knowles <benknowles@gmail.com>
2019-10-06 17:34:08 +1100

    Refactored audio_offset_finder to better handle batch processing

--- audio_offset_finder/audio_offset_finder.py
+++ audio_offset_finder/audio_offset_finder.py
@@ -36,39 +36,27 @@ def add_feature(mfcc1, rmsa1):
     tmfcc1[0:n,m:m+w]   = np.transpose(rmsa1[0:w,0:n])
     return tmfcc1
 
-def find_offset(file1, file2, fs=8000, trim=60*15, correl_nframes=1000, plotit=True):
+def get_audio(file1, fs=8000, trim=60*15):
     sr = fs
     tmp1 = convert_and_trim(file1, fs, trim)
-    tmp2 = convert_and_trim(file2, fs, trim)
     # Removing warnings because of 18 bits block size
     # outputted by ffmpeg
     # https://trac.ffmpeg.org/ticket/1843
     warnings.simplefilter("ignore", wavfile.WavFileWarning)
     a1 = wavfile.read(tmp1, mmap=True)[1] / (2.0 ** 15)
-    a2 = wavfile.read(tmp2, mmap=True)[1] / (2.0 ** 15)
     # We truncate zeroes off the beginning of each signals
     # (only seems to happen in ffmpeg, not in sox)
     a1 = ensure_non_zero(a1)
-    a2 = ensure_non_zero(a2)
-    print("Ref samples: %s Find samples: %s" % (a1.shape[0],a2.shape[0]))
+    print("%s samples: %s" % (file1,a1.shape[0]))
     mfcc1 = mfcc(a1, nwin=256, nfft=512, fs=fs, nceps=26)[0]
-    mfcc2 = mfcc(a2, nwin=256, nfft=512, fs=fs, nceps=26)[0]
     mfcc1 = std_mfcc(mfcc1)
-    mfcc2 = std_mfcc(mfcc2)
     rmsa1 = librosa.feature.rms(a1)
-    rmsa2 = librosa.feature.rms(a2)
     cent1 = librosa.feature.spectral_centroid(y=a1, sr=fs)
-    cent2 = librosa.feature.spectral_centroid(y=a2, sr=fs)
     rolloff1 = librosa.feature.spectral_rolloff(y=a1, sr=fs, roll_percent=0.1)
-    rolloff2 = librosa.feature.spectral_rolloff(y=a2, sr=fs, roll_percent=0.1)
     chroma_cq1 = librosa.feature.chroma_cqt(y=a1, sr=fs, n_chroma=10)
-    chroma_cq2 = librosa.feature.chroma_cqt(y=a2, sr=fs, n_chroma=10)
     
     onset_env1 = librosa.onset.onset_strength(y=a1, sr=sr)
-    onset_env2 = librosa.onset.onset_strength(y=a2, sr=sr)
     pulse1 = librosa.beat.plp(onset_envelope=onset_env1, sr=sr)
-    pulse2 = librosa.beat.plp(onset_envelope=onset_env2, sr=sr)
-
 
     mfcc1 = add_feature(mfcc1, rmsa1)
     mfcc1 = add_feature(mfcc1, rolloff1/fs)
@@ -77,12 +65,11 @@ def find_offset(file1, file2, fs=8000, trim=60*15, correl_nframes=1000, plotit=T
     mfcc1 = add_feature(mfcc1, onset_env1.reshape(1,onset_env1.shape[0]))
     mfcc1 = add_feature(mfcc1, pulse1.reshape(1,onset_env1.shape[0]))
 
-    mfcc2 = add_feature(mfcc2, rmsa2)
-    mfcc2 = add_feature(mfcc2, rolloff2/fs)
-    mfcc2 = add_feature(mfcc2, cent2/fs)
-    mfcc2 = add_feature(mfcc2, chroma_cq2)
-    mfcc2 = add_feature(mfcc2, onset_env2.reshape(1,onset_env2.shape[0]))
-    mfcc2 = add_feature(mfcc2, pulse2.reshape(1,onset_env2.shape[0]))
+    return tmp1, mfcc1, a1, rmsa1
+
+def find_offset(audio1, audio2, fs=8000, correl_nframes=1000, plotit=False):
+    tmp1, mfcc1, a1, rmsa1 = audio1
+    tmp2, mfcc2, a2, rmsa2 = audio2
 
     c = cross_correlation(mfcc1, mfcc2, nframes=correl_nframes)
 
@@ -90,10 +77,7 @@ def find_offset(file1, file2, fs=8000, trim=60*15, correl_nframes=1000, plotit=T
     # # The MFCC window overlap is hardcoded in scikits.talkbox
     # # offset = max_k_index * 160.0 / float(fs) # * over / sample rate
     offset = max_k_index * (a1.shape[0]/rmsa1.shape[1]) / float(fs) # * over / sample rate
-    print("Best matching window: %s" % max_k_index)
     score = (c[max_k_index] - np.mean(c)) / np.std(c) # standard score of peak
-    os.remove(tmp1)
-    os.remove(tmp2)
     if plotit:
         plt.figure(figsize=(8, 4))
         plt.plot(c)
@@ -152,3 +136,35 @@ def convert_and_trim(afile, fs, trim):
     if not psox.returncode == 0:
         raise Exception("FFMpeg failed")
     return tmp_name
+
+class BatchOffsetFinder:
+    def __init__(self, haystack_filenames, fs=8000, trim=60*15, correl_nframes=1000):
+        self.fs = fs
+        self.trim = trim
+        self.correl_nframes = correl_nframes
+        self.haystacks = []
+
+        for filename in haystack_filenames:
+            self.haystacks.append((filename, get_audio(filename, fs, trim)))
+
+    def find_offset(self, needle):
+        best_score = 0
+        best_filename = ""
+        best_offset = 0
+        needle_audio = get_audio(needle, self.fs, self.trim)
+        for (haystack_filename, haystack_audio) in self.haystacks:
+            offset, score = find_offset(haystack_audio, needle_audio, self.fs, self.correl_nframes)
+            if (score > best_score):
+                best_score = score
+                best_filename = haystack_filename
+                best_offset = offset
+
+        print("Cleaning up %s" % str(needle_audio[0]))
+        os.remove(needle_audio[0])
+
+        return best_filename, best_offset, best_score
+
+    def __del__(self):
+        for haystack in self.haystacks:
+            print("Cleaning up %s" % str(haystack[1][0]))
+            os.remove(haystack[1][0])
